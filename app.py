a52c136c70b9efa638b8390763f63f9268eae929
Pawl1973 <pmanaugh@hotmail.com>
2021-11-09 21:09:44 +1100

    change Postgres env variable5

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
 # if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
+uri = uri.replace("postgres://", "postgresql://", 1)
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\pama\\PycharmProjects\\pythonProject\\API8\\data.db'
